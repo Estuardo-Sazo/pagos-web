505346caa50b3adb55314f98819a5f2bec09cc58
Estuardo-Sazo <estuardo1206@gmail.com>
2021-06-04 23:36:57 -0600

    add Footer

--- public/utils/footer.php
+++ public/utils/footer.php
@@ -1,16 +1,19 @@
-
-
 <?php
 
 function footer($page){
+    
     ?>
-    <script src="../../assets/js/jquery.min.js"></script>
-    <script src="../../assets/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/jquery.serializejson.js"></script>
+<div class="fixed-bottom bg-dark">
+    <h6 class="text-white mt-1" align="center"> App - Jaime Sazo V 1.2</h6>
+</div>
+<script src="../../assets/js/jquery.min.js"></script>
+<script src="../../assets/js/bootstrap.min.js"></script>
+<script src="../../assets/js/jquery.serializejson.js"></script>
 
-    <script src="../../assets/js/<?=$page?>.js"></script>
+<script src="../../assets/js/<?=$page?>.js"></script>
 
 </body>
+
 </html>
 
 <?php
